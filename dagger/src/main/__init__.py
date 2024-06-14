8590131f4363bd494f59f8bf263d23aa75d6902b
andreesg <andre@goncalves.me>
2024-06-14 11:28:19 +0100

    chore: simplify pipeline with daggerverse

--- dagger/src/main/__init__.py
+++ dagger/src/main/__init__.py
@@ -26,43 +26,40 @@ class HelloDagger:
         """Publish the application container after building and testing it on-the-fly"""
         self.test(source)
         return await self.build(source).publish(
-            f"ttl.sh/myapp-{random.randrange(10 ** 8)}"
+            f"ttl.sh/hello-dagger-{random.randrange(10 ** 8)}"
+        )
+    
+    @function
+    async def test(self, source: dagger.Directory) -> str:
+        """Return the result of running unit tests"""
+        return await (
+            dag.node(ctr=self.build_env(source))
+            .commands()
+            .run(["test:unit", "run"])
+            .stdout()
         )
     
     @function
     def build(self, source: dagger.Directory) -> dagger.Container:
         """Build the application container"""
         build = (
-            self.build_env(source)
-            .with_exec(["npm", "run", "build"])
+            dag.node(ctr=self.build_env(source))
+            .commands()
+            .run(["build"])
             .directory("./dist")
         )
         return (
             dag.container()
             .from_("nginx:1.25-alpine")
             .with_directory("/usr/share/nginx/html", build)
-            .with_exposed_port(8080)
+            .with_exposed_port(80)
         )
 
     @function
-    async def test(self, source: dagger.Directory) -> str:
-        """Return the result of running unit tests"""
-        return await (
-            self.build_env(source)
-            .with_exec(["npm", "run", "test:unit", "run"])
-            .stdout()
-        )
-    
-    @function
     def build_env(self, source: dagger.Directory) -> dagger.Container:
         """Build a ready-to-use development environment"""
-        node_cache = dag.cache_volume("node")
         return (
-            dag.container()
-            .from_("node:21-slim")
-            .with_directory("/src", source)
-            .with_mounted_cache("/src/node_modules", node_cache)
-            .with_workdir("/src")
-            .with_exec(["npm", "install"])
+            dag.node(version="21").with_npm().with_source(source).install().container()
         )
+
 
